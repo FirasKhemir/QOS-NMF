bdc2e0eaf53f7ff734d3169515746f67347442f8
FirasKhemir <firas.khemir@etudiant-enit.utm.tn>
2023-02-27 15:35:12 +0100

    Added Vbarre Plus

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ Ro = float('inf')
 t = 0
 while 1:
     t += 1
-    Y = np.dot( Vbarre.T, Vbarre)
+    VbarrePlus = np.where(Vbarre < 0, 0, Vbarre)
+    Y = np.dot( Vbarre.T, VbarrePlus)
     Ro0 = Ro
     Ro = linalg.norm(Y-np.identity(Y.shape[0]),'fro')**2
     if(Ro0-Ro < sys.float_info.epsilon) or (t == Maxiter):
@@ -31,11 +32,7 @@ while 1:
     U,S,V= linalg.svd(Y)
     Vbarre = np.dot(Vbarre,np.dot(U,V.T))
     
-S = Vbarre
-print("S shape", S.shape)
-print(Vbarre.shape)
-print(M.shape)
-print(np.tril(Y).T.shape)
+S = np.where(Vbarre < 0, 0, Vbarre) #Vbarre Plus
 aux = np.dot(M,Vbarre)
 W = np.dot(aux,linalg.inv(Y.T)) # transposé de la matrice diagonale inferieure de Y
                                 # Il s'agit de l'inverse de la transposée de la matrice Y
